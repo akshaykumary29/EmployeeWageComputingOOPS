5fdb2f13ccaa32090bb75a79a8085d321e5eb5ed
akshay <akshay.yamgar29@gmail.com>
2021-10-02 20:56:08 +0530

    Added: Welcome message is added

---
+++ README.md
@@ -0,0 +1,1 @@
+Employee Wage Computing Program using OOPS
---
+++ src/main/java/com/bridgelabz/employeewagecomputing/EmployeeWageCompute.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeewagecomputing;
+
+public class EmployeeWageCompute {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computing Program.!!!");
+    }
+}
