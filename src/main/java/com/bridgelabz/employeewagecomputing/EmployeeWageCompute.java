eba8c8b170279447f25132f5ab66260ab730cbfb
akshay <akshay.yamgar29@gmail.com>
2021-10-02 22:55:06 +0530

    Added: Check employee present or absent

--- src/main/java/com/bridgelabz/employeewagecomputing/EmployeeWageCompute.java
+++ src/main/java/com/bridgelabz/employeewagecomputing/EmployeeWageCompute.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.employeewagecomputing;
 
+/**
+ * Employee Wage Compute Program Using OOPS Concept
+ */
+
+import java.util.Random;
+
 public class EmployeeWageCompute {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computing Program.!!!");
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == 1) {
+            System.out.println("Employee is present for work ");
+        }
+        else {
+            System.out.println("Employee is absent for work ");
+        }
     }
 }
