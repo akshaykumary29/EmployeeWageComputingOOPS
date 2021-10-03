f2ecbd4a854c03929c345571e0951e36d9d4564f
akshay <akshay.yamgar29@gmail.com>
2021-10-03 23:49:41 +0530

    Added: employeeWageBuilder Class is added

--- src/main/java/com/bridgelabz/employeewagecomputing/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/employeewagecomputing/EmployeeWageBuilder.java
@@ -6,20 +6,23 @@ package com.bridgelabz.employeewagecomputing;
 
 import java.util.Random;
 
-public class EmployeeWageCompute {
+public class EmployeeWageBuilder {
     static final int WAGE_PER_HR = 20;
     static final int FULL_DAY_HR = 8;
     static final int PART_TIME_HR = 4;
     static final int MAX_WORKING_DAYS = 20;
     static final int MAX_HRS_PER_MONTH = 100;
+    static final int IS_FULL_TIME = 0;
+    static final int IS_PART_TIME = 1;
 
-    public static void main(String[] args) {
+    public void employeeWage() {
         System.out.println("Welcome to Employee Wage Computing Program.!!!");
         int dailyEmpWage;
         Random random = new Random();
         int totalWage = 0;
         int totalWorkingHrs = 0;
         int totalWorkingDays = 0;
+        int empHours = 0;
 
         while (totalWorkingHrs <= MAX_HRS_PER_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
             totalWorkingDays++;
@@ -31,6 +34,7 @@ public class EmployeeWageCompute {
                     System.out.println("Employee daily wage on day is :" + dailyEmpWage);
                     totalWage += dailyEmpWage;
                     totalWorkingDays += FULL_DAY_HR;
+                    empHours = FULL_DAY_HR;
                     break;
                 case 1:
                     System.out.println("Employee present for Halfday (Part time)");
@@ -38,13 +42,25 @@ public class EmployeeWageCompute {
                     System.out.println("Employee Wage is :" + dailyEmpWage);
                     totalWage += dailyEmpWage;
                     totalWorkingHrs += PART_TIME_HR;
+                    empHours = PART_TIME_HR;
                     break;
                 case 2:
                     System.out.println("Employee is absent for work on day");
+                    empHours = 0;
             }
-            System.out.println("Total Employee Wage : " + totalWage);
-            System.out.println("Number of Working Days : " + totalWorkingDays);
-            System.out.println("Number of Working Hours : " + totalWorkingHrs);
+            totalWorkingHrs += empHours;
         }
+        totalWage = totalWorkingHrs * WAGE_PER_HR;
+        System.out.println("Total Employee Wage : " + totalWage);
+        System.out.println("Number of Working Days : " + totalWorkingDays);
+        System.out.println("Number of Working Hours : " + totalWorkingHrs);
+    }
+
+    public static void main(String[] args) {
+        //Creating object
+        EmployeeWageBuilder empWage = new EmployeeWageBuilder();
+        //calling the method
+        empWage.employeeWage();
     }
 }
+
